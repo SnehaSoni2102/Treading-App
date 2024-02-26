2d80ccd636f44131eccbadc186656301e44e9f98
sneha <snehasoni2102@gmail.com>
2024-02-26 20:31:18 +0530

    LikeChart Integerated with API

--- src/components/Body/BodyComponents/LikeChart.jsx
+++ src/components/Body/BodyComponents/LikeChart.jsx
@@ -9,10 +9,10 @@ export const LikeChart = () => {
   const [loading, setLoading] = useState(true);
 
   const sliderSettings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 6,
     slidesToScroll: 1,
   };
 
@@ -38,25 +38,33 @@ export const LikeChart = () => {
 
   return (
     <React.Fragment>
-      <div className="bg-white">
+      <div className="bg-gray-400">
         <div className=" w-11/12 m-auto">
-        <h1>You may also like</h1>
+          <h1 className="py-4 text-[24px] font-medium">You may also like</h1>
           <Slider {...sliderSettings}>
-          {data?.coins?.map((item, index) => (
-        <div key={item?.item?.coinId} className="w-[200px] h-[150px] bg-[#ffffff] gap-4">
-          <div className="m-2 bg-[#ffffff] p-4 border border-gray-400 rounded-lg">
-            <div className="flex flex-wrap justify-start items-center gap-2">
-            <img src={item?.item?.small} alt="coin" style={{ height: 24, width: 24 }} />
-          <h1>{item?.item?.symbol}</h1>
-          <div className="flex flex-wrap justify-center items-center rounded-sm bg-[#EBF9F4] text-[#14B079] text-[10px] px-1 h-[16px]">
-            <h1>{item?.item?.data?.price}</h1>
-          </div>
-            </div>
-           
-          </div>
-          
-        </div>
-      ))}
+            {data?.coins?.map((item, index) => (
+              <div
+                key={item?.item?.coinId}
+                className=" h-[px] bg-[#ffffff] gap-4"
+              >
+                <div className="m-2 bg-[#ffffff] p-4 border border-gray-400 rounded-lg">
+                  <div className="flex flex-wrap justify-start items-center gap-2 text-[16px]">
+                    <img
+                      src={item?.item?.small}
+                      alt="coin"
+                      style={{ height: 26, width: 26 }}
+                    />
+                    <h1 className="text-[16px]">{item?.item?.symbol}</h1>
+                    <div className="flex flex-wrap justify-center items-center rounded-sm bg-[#EBF9F4] text-[#14B079] text-[10px] px-1 h-[16px]">
+                      <h1>{item?.item?.data?.price}</h1>
+                    </div>
+                  </div>
+                  <div className="p-2 mx-auto">
+                  <img src={item?.item?.data?.sparkline} alt="coin" style={{ height: 80, width: 100 }} />
+                  </div>
+                </div>
+              </div>
+            ))}
           </Slider>
         </div>
       </div>
