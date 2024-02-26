105bd884c453118714980549ab9ca346bd1b7f0a
sneha <snehasoni2102@gmail.com>
2024-02-27 01:03:19 +0530

    trading view cHART ISSUE RESOLVED

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -10,12 +10,12 @@ export const Body = () => {
   return (
     <React.Fragment>
       <div className="flex flex-row justify-center gap-4 m-[56px]">
-        {/* <div className="bg-red-100 w-[881px] h-[711px]">
+        <div className="bg-red-100 w-[881px] h-[711px]">
          <TradingViewWidget /> 
-      </div> */}
-        <div>
+      </div>
+        {/* <div>
           <img src={div} alt="trade" style={{ height: 711, width: 881 }} />
-        </div>
+        </div> */}
         <div className=" flex flex-col gap-4">
           <KoinXFreeCard />
           <TreandingCoins />
