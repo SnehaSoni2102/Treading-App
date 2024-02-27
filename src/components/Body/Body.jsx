b21e9bee7c4e3217a5e3d463f54a08ced79d270c
sneha <snehasoni2102@gmail.com>
2024-02-27 05:47:25 +0530

    final commit

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { KoinXFreeCard } from "./BodyComponents/koinXFreeCard";
 import TradingViewWidget from "./BodyComponents/TradingViewWidget";
-import div from "../../Assets/div.svg";
+import before from "../../Assets/before.svg";
 import { TreandingCoins } from "./BodyComponents/TreandingCoins";
 import { LikeChart } from "./BodyComponents/LikeChart";
 import { TrendingCoinCarousel } from "./BodyComponents/TrendingCoinCarousel";
@@ -9,6 +9,11 @@ import { TrendingCoinCarousel } from "./BodyComponents/TrendingCoinCarousel";
 export const Body = () => {
   return (
     <React.Fragment>
+      <div className="flex flex-row justify-start m-4 gap-2">
+        <h1 className="text-[12px] text-gray-400">Cryptocurrencies</h1>
+        <img className="mt-1.5" src={before} alt="coin" style={{ height: 8, width: 8 }} />
+        <h1 className="text-[12px]">Bitcoin</h1>
+      </div>
       <div className="flex flex-row justify-center gap-4 md:m-[56px] m-4">
         <div className="bg-red-100 md:w-[881px] w-full  md:h-[711px] h-[491px]">
          <TradingViewWidget /> 
